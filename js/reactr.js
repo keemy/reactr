97960e7678fdfde035e59930329e5c10b74ba3b9
keemy <devin1891@gmail.com>
2014-01-12 10:53:11 -0800

    added most of the logic for timing and display
    looks like a state machine i work 2 hard

--- js/reactr.js
+++ js/reactr.js
@@ -37,14 +37,102 @@ $( document ).ready(function() {
 		$("#Tests").append("<option>"+listOfTests[i]+"</option>");
 	}
 	
-	$(".test").append('<div class="testtime"></div>');
-	$(".testtime").append("<p>click to start</p>");
+	$(".test").append('<div class="testtime start"></div>');
+	$(".testtime").append("<p1>click to start</p1>");
+	$(".testtime").append("<p2 style='display: none;'>wait for the Cue</p2>");
+	$(".testtime").append("<p3 style='display: none;'>Too Soon</p3>");
+	$(".testtime").append("<p4 style='display: none;'>GO!!</p4>");
+	$(".testtime").append("<p5 style='display: none;'></p5>");
 	//$(".testtime").click(testsCode[$(".testtime").parents("div").attr("id")]);
 	//TODO make actual tests code below used 2 submit times manually
 
-	$("#visual .testtime").click(function(){
-		$(this).toggleClass('yellow');
-		$("p").slideToggle(5); 
+	
+	var switch_random=function(){
+		timer1 = setTimeout(function() { behavior["time"](); }, 1000+Math.random()*2000);
+	};
+	var cancel_switch=function(){
+		clearTimeout(timer1);
+	}
+	
+	
+	
+	
+	var state="start";
+	var behavior={
+		start:function(){
+			var elm = $("#visual .testtime")
+			elm.toggleClass('start');
+			elm.toggleClass('wait');
+			
+			
+			$("p1").slideToggle(5);
+			$("p2").slideToggle(5);
+			
+			state="wait";
+			
+			switch_random();
+		},
+		wait:function(){
+			cancel_switch();
+			
+			var elm = $("#visual .testtime")
+			elm.toggleClass('wait');
+			elm.toggleClass('scold');
+			
+			
+			$("p2").slideToggle(5);
+			$("p3").slideToggle(5);
+			
+			state="scold";
+		},
+		scold:function(){
+			var elm = $("#visual .testtime")
+			elm.toggleClass('scold');
+			elm.toggleClass('wait');
+			
+			$("p2").slideToggle(5);
+			$("p3").slideToggle(5);
+		
+			state="wait";
+			
+			switch_random();
+		},
+		time:function(){
+			var elm = $("#visual .testtime");
+			var classes=elm.attr('class').split(" ");
+			for( var i=0; i<classes.length;i++){
+				if(classes[i]!="testtime"){
+					elm.toggleClass(classes[i]);
+				}
+			}
+			elm.toggleClass('time');
+			
+			$("p2").slideToggle(0);
+			$("p4").slideToggle(0);
+			
+
+			timeStart = performance.now();
+			state="result";
+			
+		},
+		result:function(){
+			
+			timeEnd= performance.now();			
+			lagEnd = performance.now();
+			reactionTime=Math.round(timeEnd-timeStart -(lagEnd-lagStart));
+			
+			$("p4").slideToggle(0);
+			$("p5").slideToggle(0);
+			$("p5").text(reactionTime.toString())
+		
+			
+		}
+	};
+	
+	
+	$("#visual .start").click(function(){
+		lagStart = performance.now();
+		behavior[state]();
 		
 	});
 	
