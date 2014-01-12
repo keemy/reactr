9d7b184bada997dfe4996cf55f2f43680d00506b
keemy <devin1891@gmail.com>
2014-01-12 14:53:44 -0800

    mousedown() instead of click()
    click() needs to be pressed and released noticed this added like 50~120
    ms for me and it's not really a measure of reaction time so changed to
    mousedown

--- js/reactr.js
+++ js/reactr.js
@@ -151,7 +151,7 @@ $( document ).ready(function() {
 	};
 	
 	
-	$("#visual .start").click(function(){
+	$("#visual .start").mousedown(function(){
 		lagStart = performance.now();
 		behavior[state]();
 		
