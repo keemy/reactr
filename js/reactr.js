101fdce983e5617c1943ea018df59e2a949d6a28
keemy <devin1891@gmail.com>
2014-01-12 22:09:42 -0800

    added 2nd visual test as well as sound test

--- js/reactr.js
+++ js/reactr.js
@@ -1,4 +1,4 @@
-var listOfTests=["sound","visual","visual222"];
+var listOfTests=["sound","visual","visual2"];
 var userName=prompt("whats ur name");
 
 function setCookie(c_name,value,exdays){
@@ -43,124 +43,9 @@ $( document ).ready(function() {
 	$(".testtime").append("<p class='scoldText' style='display: none;'>Too Soon</p>");
 	$(".testtime").append("<p class='timeText' style='display: none;'>GO!!</p>");
 	$(".testtime").append("<p class='resultText' style='display: none;'></p>");
-	//$(".testtime").click(testsCode[$(".testtime").parents("div").attr("id")]);
-	//TODO make actual tests code below used 2 submit times manually
+	
+	
 
-	
-	var switch_random=function(){
-		timer1 = setTimeout(function() { behavior["wait2"](); }, 1500+Math.random()*3000);
-	};
-	var cancel_switch=function(){
-		clearTimeout(timer1);
-	}
-	var clear_classes=function(obj){
-		var classes=obj.attr('class').split(" ");
-		for( var i=0; i<classes.length;i++){
-				if(classes[i]!="testtime"){
-					obj.toggleClass(classes[i]);
-				}
-			}
-	
-	}
-	
-	
-	
-	
-	var state="start";
-	var behavior={
-		
-		start:function(){
-			var elm = $("#visual .testtime")
-			clear_classes(elm);
-			elm.toggleClass('wait');
-			
-			elm.find("p").hide();
-			elm.find(".waitText").show();
-			
-			
-			state="wait";
-			
-			switch_random();
-		},
-		wait:function(){
-			cancel_switch();
-			
-			var elm = $("#visual .testtime")
-			
-			clear_classes(elm);
-			elm.toggleClass('scold');
-			
-			elm.find("p").hide();
-			elm.find(".scoldText").show();
-						
-			state="scold";
-		},
-		scold:function(){
-			var elm = $("#visual .testtime")
-			
-			clear_classes(elm);
-			elm.toggleClass('wait');
-			
-			
-			elm.find("p").hide();
-			elm.find(".waitText").show();
-					
-			state="wait";
-			
-			switch_random();
-		},
-		wait2:function(){
-			var elm = $("#visual .testtime");
-			clear_classes(elm);
-			elm.toggleClass('time');
-			
-			elm.find("p").hide();
-			elm.find(".timeText").show();
-			
-			
-
-			timeStart = performance.now();
-			state="time";
-			
-		},
-		time:function(){
-			
-			timeEnd= performance.now();			
-			lagEnd = performance.now();
-			reactionTime=Math.round(timeEnd-timeStart -(lagEnd-lagStart));
-			
-			var elm = $("#visual .testtime");
-			clear_classes(elm);
-			elm.toggleClass('result');
-			
-			
-			elm.find("p").hide();
-			elm.find(".resultText").text(reactionTime.toString());
-			elm.find(".resultText").append(" click to play again");
-			elm.find(".resultText").show();
-			
-			
-			$("#results").append(reactionTime.toString());
-			$("#results").append(" ");
-			
-			state="result";
-		},
-		result:function(){
-			behavior["start"]();
-		}
-	};
-	
-	
-	$("#visual .start").mousedown(function(){
-		lagStart = performance.now();
-		behavior[state]();
-		
-	});
-	
-	
-	//visual test
-	
-	
 	// $(".test").append('<form class="testtime"></form>')
 	// $(".test .testtime").append('<input type="number " class="time"/>')
 	// $(".test .testtime").append('<input type="submit" class="submit" value="Go"/>')
@@ -184,4 +69,5 @@ $( document ).ready(function() {
 	
 	
 });
+
 
---
+++ js/visual2.js
@@ -0,0 +1,111 @@
+$( document ).ready(function() {
+	
+
+	var switch_random=function(){
+		timer1 = setTimeout(function() { behavior["wait2"](); }, 1500+Math.random()*3000);
+	};
+	var cancel_switch=function(){
+		clearTimeout(timer1);
+	}
+	var clear_classes=function(obj){
+		var classes=obj.attr('class').split(" ");
+		for( var i=0; i<classes.length;i++){
+				if(classes[i]!="testtime"){
+					obj.toggleClass(classes[i]);
+				}
+			}
+	
+	}
+	
+	var state="start";
+	var behavior={
+		
+		start:function(){
+			var elm = $("#visual2 .testtime")
+			clear_classes(elm);
+			elm.toggleClass('wait');
+			
+			elm.find("p").hide();
+			elm.find(".waitText").show();
+			
+			
+			state="wait";
+			
+			switch_random();
+		},
+		wait:function(){
+			cancel_switch();
+			
+			var elm = $("#visual2 .testtime")
+			
+			clear_classes(elm);
+			elm.toggleClass('scold');
+			
+			elm.find("p").hide();
+			elm.find(".scoldText").show();
+						
+			state="scold";
+		},
+		scold:function(){
+			var elm = $("#visual2 .testtime")
+			
+			clear_classes(elm);
+			elm.toggleClass('wait');
+			
+			
+			elm.find("p").hide();
+			elm.find(".waitText").show();
+					
+			state="wait";
+			
+			switch_random();
+		},
+		wait2:function(){
+			var elm = $("#visual2 .testtime");
+			clear_classes(elm);
+			elm.toggleClass('time2');
+			
+			elm.find("p").hide();
+			elm.find(".timeText").show();
+			
+			
+
+			timeStart = performance.now();
+			state="time";
+			
+		},
+		time:function(){
+			
+			timeEnd= performance.now();			
+			lagEnd = performance.now();
+			reactionTime=Math.round(timeEnd-timeStart -(lagEnd-lagStart));
+			
+			var elm = $("#visual2 .testtime");
+			clear_classes(elm);
+			elm.toggleClass('result');
+			
+			
+			elm.find("p").hide();
+			elm.find(".resultText").text(reactionTime.toString());
+			elm.find(".resultText").append(" click to play again");
+			elm.find(".resultText").show();
+			
+			
+			$("#results").append(reactionTime.toString());
+			$("#results").append(" ");
+			
+			state="result";
+		},
+		result:function(){
+			behavior["start"]();
+		}
+	};
+	
+	
+	$("#visual2 .start").mousedown(function(){
+		lagStart = performance.now();
+		behavior[state]();
+		
+	});
+	
+});
